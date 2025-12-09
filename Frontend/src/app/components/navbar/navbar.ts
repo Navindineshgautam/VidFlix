4c3cf0313196b38d661d082099673782d6051f11
SaiVenkat.Chitturi <SaiVenkat.Chitturi@EntainIndia.com>
2025-12-09 15:46:34 +0530

    qol: Added padding to navbar-child

--- Frontend/src/app/components/navbar/navbar.ts
+++ Frontend/src/app/components/navbar/navbar.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
 export class Navbar {
+  private isDesktop: boolean = false;
 
+  private checkScreenSize(): void {
+    this.isDesktop = window.innerWidth >= 767;
+  }
+
+  public constructor() {
+    this.checkScreenSize()
+  }
+
+  public getIsDesktop(): boolean {
+    return this.isDesktop;
+  }
+
+  @HostListener("window:resize", ["$event"])
+  onResize(event: Event) {
+    this.checkScreenSize()
+  }
 }
