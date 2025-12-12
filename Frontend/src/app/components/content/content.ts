db7a1ee5bff64e9dae19ad3828c7def6c7b65904
SaiVenkat.Chitturi <SaiVenkat.Chitturi@EntainIndia.com>
2025-12-12 16:36:09 +0530

    Added Video Grid based on viewports

--- Frontend/src/app/components/content/content.ts
+++ Frontend/src/app/components/content/content.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 
+import { Video } from './components/video/video';
+import { WindowService } from '../../services/window/window.service';
+
 @Component({
   selector: 'app-content',
-  imports: [],
+  imports: [Video],
   templateUrl: './content.html',
   styleUrl: './content.css',
 })
 export class Content {
+  public dummyVideos: number[] = [...Array(50).keys()]
 
+  public constructor(public windowService: WindowService) {}
 }
